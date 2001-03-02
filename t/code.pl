e121b0e0d5b6faf87081d45a08c267e8c3891a02
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2001-03-02 18:05:24 +0000

    Import of RAM/Getargs-Long-0.1.1 from CPAN.
    gitpan-cpan-distribution: Getargs-Long
    gitpan-cpan-version:      0.1.1
    gitpan-cpan-path:         RAM/Getargs-Long-0.1.1.tar.gz
    gitpan-cpan-author:       RAM
    gitpan-cpan-maturity:     released

---
+++ t/code.pl
@@ -0,0 +1,43 @@
+#
+# $Id: code.pl,v 0.1.1.1 2001/03/02 17:59:48 ram Exp $
+#
+#  Copyright (c) 2000-2001, Raphael Manfredi
+#  
+#  You may redistribute only under the terms of the Artistic License,
+#  as specified in the README file that comes with the distribution.
+#
+# HISTORY
+# $Log: code.pl,v $
+# Revision 0.1.1.1  2001/03/02 17:59:48  ram
+# patch1: added.
+#
+# Revision 0.1  2001/03/01 14:46:55  manfredi
+# Initial revision.
+#
+# $EndLog$
+#
+
+sub ok {
+	my ($num, $ok) = @_;
+	print "not " unless $ok;
+	print "ok $num\n";
+}
+
+sub contains {
+	my ($file, $pattern) = @_;
+	local *FILE;
+	local $_;
+	open(FILE, $file) || die "can't open $file: $!\n";
+	my $found = 0;
+	while (<FILE>) {
+		if (/$pattern/) {
+			$found = 1;
+			last;
+		}
+	}
+	close FILE;
+	return $found;
+}
+
+1;
+
